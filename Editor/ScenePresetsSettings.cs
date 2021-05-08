f606cfdc4df94cc79f4e47dda936dc95284f6b1c
branchpanic <branchpanic@gmail.com>
2021-05-08 13:17:33 -0400

    Implement working prototype

---
+++ Editor/ScenePresetsSettings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+namespace ScenePresets.Editor
+{
+    public class ScenePresetsSettings : ScriptableObject
+    {
+        [Serializable]
+        public struct Preset
+        {
+            public string name;
+            public SceneSetup[] scenes;
+        }
+
+        public List<Preset> presets;
+    }
+}
